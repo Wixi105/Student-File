cf793ba9fb7f76823705750f5b605f6d137898fe
wixi105 <ericsampson105@gmail.com>
2022-07-21 13:06:01 +0000

    working on changes
    Signed-off-by: wixi105 <ericsampson105@gmail.com>

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            Welcome to your {{ __('Dashboard') }}, <span class="text-blue-800"> {{ Auth::user()->name }}</span>
         </h2>
     </x-slot>
 
@@ -14,4 +14,10 @@
             </div>
         </div>
     </div>
+
+    {{-- Student List Start. --}}
+    <div>
+
+    </div>
+    {{-- Student List End. --}}
 </x-app-layout>
