7ec67bce00f31465b87427af0e3af143aa9fef25
wixi105 <ericsampson105@gmail.com>
2022-07-22 14:13:03 +0000

    working on changes
    Signed-off-by: wixi105 <ericsampson105@gmail.com>

---
+++ resources/views/components/table.blade.php
@@ -0,0 +1,54 @@
+<!-- component -->
+<!-- This is an example component -->
+<div class="w-full mt-24 mx-auto">
+
+    <div class="flex flex-col">
+        <div class="overflow-x-auto shadow-md sm:rounded-lg">
+            <div class="inline-block min-w-full align-middle">
+                <div class="overflow-hidden ">
+                    <table class="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-700">
+                        <thead class="bg-white ">
+                            <tr>
+
+                                <th scope="col"
+                                    class="py-3 px-6 text-sm font-medium tracking-wider text-left text-gray-700 uppercase ">
+                                    Student ID
+                                </th>
+                                <th scope="col"
+                                    class="py-3 px-6 text-sm font-medium tracking-wider text-left text-gray-700 uppercase">
+                                    First Name
+                                </th>
+                                <th scope="col"
+                                    class="py-3 px-6 text-sm font-medium tracking-wider text-left text-gray-700 uppercase">
+                                    Last Name
+                                </th>
+                                <th scope="col" class="p-4">
+                                    <span class="sr-only">View</span>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody class="bg-gray-500 divide-y divide-gray-200 dark:divide-gray-700">
+                            <tr class="hover:bg-gray-100">
+
+                                <td
+                                    class="py-4 px-6 text-lg font-medium text-gray-100  whitespace-nowrap tracking-wider">
+                                    PS/ITC/17/0019</td>
+                                <td
+                                    class="py-4 px-6 text-lg font-medium text-gray-100 whitespace-nowrap tracking-wider">
+                                    Michael</td>
+                                <td
+                                    class="py-4 px-6 text-lg font-medium text-gray-100  whitespace-nowrap tracking-wider">
+                                    Scott</td>
+                                <td class="py-4 px-6 text-md text-right whitespace-nowrap">
+                                    <a href="#"
+                                        class="text-gray-100 font-bold hover:underline uppercase tracking-widest">View</a>
+                                </td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
